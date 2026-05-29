721253d9ceb6ddc8ca1c13c6ac134ecedc530d49
DESKTOP-ICAAMJK\adi <adi.sukawana@kudosity.com>
2026-05-29 14:47:45 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const SEQUENCER_API_URL = process.env.SEQUENCER_API_URL || "http://stage.sqn.cr";
+const SEQUENCER_API_URL = process.env.NEXT_PUBLIC_SEQUENCER_API_URL || "http://stage.sqn.cr";
 
 const nextConfig: NextConfig = {
   async rewrites() {
